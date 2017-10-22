b741683f2edfadab3ae81d73c9fd9d4fd77dba03
smaine M <contact@smaine.me>
2017-10-22 12:22:45 +0200

    label edited

--- Emoji Dictionnary/Assets.xcassets/MainController.swift
+++ Emoji Dictionnary/Assets.xcassets/MainController.swift
@@ -11,22 +11,20 @@ import UIKit
 class MainController: UIViewController {
     @IBOutlet weak var englishBtn: UIButton!
     
-<<<<<<< HEAD
+
     @IBOutlet weak var spanishBtn: UIButton!
-=======
->>>>>>> 7dea19e9522b1c89c10df7ba199e54627b85f611
+
     @IBOutlet weak var frenchBtn: UIButton!
     @IBOutlet weak var arabicBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-<<<<<<< HEAD
+
     @IBAction func spanishActionBtn(_ sender: Any) {
          sendLanguageToViewController(language: "spanish")
     }
-=======
->>>>>>> 7dea19e9522b1c89c10df7ba199e54627b85f611
+
     @IBAction func englishActionBtn(_ sender: Any) {
         sendLanguageToViewController(language: "english")
     }
